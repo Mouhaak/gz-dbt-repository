1b98459161107823599b91549163c202612971fb
Mouhamed Kartali <mouhamedkartali5@gmail.com>
2024-02-06 17:35:50 +0000

    margin and purchase_cost added!

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,8 @@
+select 
+t1.*, 
+t2.purchase_price, 
+t1.quantity * t2.purchase_price AS purchase_cost,
+t1.revenue - t2.purchase_price AS margin
+from {{ref("stg_raw__sales")}} AS t1
+join {{ref("stg_raw__product")}} AS t2
+using (products_id)
