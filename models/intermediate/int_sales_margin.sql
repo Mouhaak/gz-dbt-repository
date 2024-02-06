14c039baed404c6909083adeb2244e998c63bd57
Mouhamed Kartali <mouhamedkartali5@gmail.com>
2024-02-06 18:14:29 +0000

    por si acaso

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,5 +4,5 @@ t2.purchase_price,
 t1.quantity * t2.purchase_price AS purchase_cost,
 t1.revenue - t2.purchase_price AS margin
 from {{ref("stg_raw__sales")}} AS t1
-join {{ref("stg_raw__product")}} AS t2
+left join {{ref("stg_raw__product")}} AS t2
 using (products_id)
